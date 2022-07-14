2c3f6be4c2609eafa1987a9a80f1da17965e44fb
Nyyu <89911360+Nyyu@users.noreply.github.com>
2022-07-14 04:25:47 -0300

    finish header + sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react"
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Icon, Input, Text, Avatar, HStack, Box } from "@chakra-ui/react"
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
 
 function Header() {
   return (
@@ -52,6 +52,34 @@ function Header() {
           as={RiSearchLine}
           fontSize="20"
         />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing='8'
+          mx="8"
+          pr="8"
+          py="1"
+          borderRight={1}
+          borderColor='gray.700'
+          color="gray.300"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex
+          align='center'
+        >
+          <Box mr='4' textAlign='right' >
+            <Text>NyuuSz</Text>
+            <Text color='gray.300' fontSize='small' >nyyu.dev@gmail.com</Text>
+          </Box>
+          <Avatar size='md' name="NyuuSz" src="https://github.com/Nyyu.png" />
+        </Flex>
       </Flex>
     </Flex>
   )
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,99 @@
-import React from 'react'
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react"
+import dynamic from "next/dynamic"
+
 import Header from "../components/Header"
+import Sidebar from "../components/Sidebar"
+
+import { ApexOptions } from 'apexcharts'
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false
+})
+
+const options: ApexOptions = {
+  chart: {
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    }
+  },
+  grid: {
+    show: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  tooltip: {
+    theme: 'dark'
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[500]
+    },
+    axisTicks: {
+      color: theme.colors.gray[500]
+    },
+    categories: [
+      '2021-03-18T00:00:00.000Z',
+      '2021-03-19T00:00:00.000Z',
+      '2021-03-20T00:00:00.000Z',
+      '2021-03-21T00:00:00.000Z',
+      '2021-03-22T00:00:00.000Z',
+      '2021-03-23T00:00:00.000Z',
+      '2021-03-24T00:00:00.000Z',
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3
+    }
+  }
+}
+const series = [{
+  name: 'weekly subs',
+  data: [102, 23, 123, 20, 1203, 203, 129]
+}]
 
 function dashboard() {
   return (
-    <>
+    <Flex direction='column' h='100vh'>
       <Header />
-    </>
+
+      <Flex
+        w='100%'
+        px='6'
+        my='6'
+        mx='auto'
+        maxWidth={1480}
+      >
+        <Sidebar />
+
+        <SimpleGrid minChildWidth='320px' flex='1' gap='4' alignContent='flex-start'>
+          <Box
+            p='8'
+            bg='gray.800'
+            borderRadius={8}
+          >
+            <Text fontSize='lg' mb='4' >Weekly subscribers</Text>
+            <Chart height={160} series={series} options={options} type='area' />
+          </Box>
+          <Box
+            p='8'
+            bg='gray.800'
+            borderRadius={8}
+          >
+            <Text fontSize='lg' mb='4' >Weekly subscribers</Text>
+            <Chart height={160} series={series} options={options} type='area' />
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex >
   )
 }
 
